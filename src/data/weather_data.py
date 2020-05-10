0a3ba3dc78d42746eed835f7a2ce372c318eb59d
worasom <worasom@gmail.com>
2020-05-10 09:10:49 -0700

    auto backup

---
+++ src/data/weather_data.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+from ..imports import *
+
+"""Scrape weather data and weather information 
+
+"""
+
+# find station in the city name 
+def find_weather_stations(city_names:list, weather_json_file:str):
+    """Find a list of weather stations in city_names list
+    """
+    # load weather information
+    with open(weather_json_file,'r') as f:
+        station_dict_list = json.load(f)
+        
+    weather_stations_info = []
+    for city_name in city_names: 
+        for i, station in enumerate(station_dict_list):
+            if city_name in station['city_name']:
+                weather_stations_info.append(station)
+                
+    return weather_stations_info
+
+def get_data_n_soup(browser, date_str,header_url,waittime=30):
+    ''' Input: date in string
+    - Ask Selenium to open the website, and execute inner javascript.
+    - Parse data into beautifulsoup object and look for the hourly table
+    - Parse the table into a panda dataframe
+    - remove the unit 
+    - add date column
+    
+    return: daily weather dataframe and beauitfulsoup object of that table
+    '''
+    #url=f'https://www.wunderground.com/history/daily/th/bang-phut/VTBD/date/{date_str}'
+    url = header_url+date_str
+    #print(url)
+    browser.get(url)
+    time.sleep(waittime)
+    innerhtml= browser.execute_script("return document.body.innerHTML")
+    soup = BeautifulSoup(innerhtml,features="lxml")
+    div_table = soup.find_all('table')
+    daily_df = pd.read_html(str(div_table))[-1]
+     
+    
+    # add date columns
+    daily_df['datetime'] = pd.to_datetime(date_str + ' ' +daily_df['Time'], format="%Y-%m-%d %I:%M %p")
+    return daily_df, div_table
+
+def convert_temp_col(data_df, temperature_col):
+    # convert string temperature in F to celcious in float 
+    for col in temperature_col:
+        if col in data_df.columns:
+            data_series = data_df[col].copy()
+            data_series = data_series.str.replace('F','')
+            data_series = data_series.astype(float)
+            data_series = ((data_series - 32)*5/9).round(2)
+            data_df[col] = data_series
+            data_df.columns = data_df.columns.str.replace(col, col+'(C)')
+            
+    return data_df
+
+def convert_wind_col(data_df, win_col):
+    # convert string wind speed and wind gust in mph to kph 
+    for col in win_col:
+        if col in data_df.columns:
+            data_series = data_df[col].copy()
+            # remove unit in the data 
+            data_series = data_series.str.replace('mph','')
+            data_series = data_series.str.replace(',','')
+            data_series = data_series.astype(float)
+            # convert the value
+            data_series = (data_series*1.60934).round(0)
+            data_df[col] = data_series
+            data_df.columns = data_df.columns.str.replace(col, col+'(kmph)')
+            
+    return data_df
+
+def convert_pressure_col(data_df, pressure_col):
+    # convert string pressure in 'inch' to float 
+    for col in pressure_col:
+        if col in data_df.columns:
+            data_series = data_df[col].copy()
+            data_series = data_series.str.replace('in','')
+            data_series = data_series.astype(float)
+            data_df[col] = data_series
+            data_df.columns = data_df.columns.str.replace(col, col+'(in)')
+            
+    return data_df
+
+def convert_humidity_col(data_df, humidity_col):
+    # convert string temperature in F to celcious in float 
+    for col in humidity_col:
+        if col in data_df.columns:
+            data_series = data_df[col].copy()
+            data_series = data_series.str.replace('%','')
+            data_series = data_series.astype(int)
+            data_df[col] = data_series
+            data_df.columns = data_df.columns.str.replace(col, col+'(%)')
+            
+    return data_df
+
+def convert_unit(data_df):
+    # convert string data into number by removing the text in the unit. Put the text in the columns name.
+    # convert temperature wand windspeed into metric system
+    temperature_col = ['Temperature','Dew Point']
+    wind_col = ['Wind Speed','Wind Gust']
+    pressure_col = ['Pressure','Precip.']
+    humidity_col = ['Humidity']
+    data_df = convert_temp_col(data_df, temperature_col)
+    data_df = convert_wind_col(data_df, wind_col)
+    data_df = convert_pressure_col(data_df,pressure_col)
+    data_df = convert_humidity_col(data_df, humidity_col)
+    return data_df
+
+def scrape_weather( city_json, date_range):
+    # scrape weather data from a city in date in date range 
+    # save filename using city name 
+    # return weather data and bad data df and date that fail 
+    # append the data to an exsiting file if the files does not exists
+    browser = webdriver.Firefox()
+    time.sleep(2)
+    weather = pd.DataFrame()
+    bad_date_df = pd.DataFrame()
+    
+    # Build header URL 
+    specific_url = city_json['specific_url']
+    header_url = 'https://www.wunderground.com/history/daily/' + specific_url + 'date/'
+     
+    
+    for i, date in enumerate(date_range):
+    
+        try:
+            # obtain daily weather dataframe
+            daily_df, div_table = get_data_n_soup(browser,date,header_url=header_url,waittime=30)
+        
+        except:
+            # fail query, 
+            bad_date_df = pd.concat([bad_date_df, pd.DataFrame({'header_url':'https://www.wunderground.com/history/daily/th/mueang-chiang-mai/VTCC/date/',
+                                                    'date': date},index=[0])],ignore_index=True)
+        
+        else: 
+            if len(daily_df)==0:
+                    # fail query, 
+                bad_date_df = pd.concat([bad_date_df, pd.DataFrame({'header_url':'https://www.wunderground.com/history/daily/th/mueang-chiang-mai/VTCC/date/',
+                                                    'date': date},index=[0])],ignore_index=True)
+            else:
+                # good query 
+                # convert unit of the data 
+                daily_df = convert_unit(daily_df)
+                #combine the weather for each day
+                weather = pd.concat([weather,daily_df], axis=0, join='outer')
+ 
+    browser.close()
+
+    return weather, bad_date_df
+
+
+
