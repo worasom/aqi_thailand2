3b775cbf58f97f41707696a376aaaf628c937a7f
worasom <worasom@gmail.com>
2020-05-31 11:25:17 -0700

    2020-05-31 11:25 auto backup

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -32,3 +32,50 @@ def add_is_holiday(df,holiday_file='C:/Users/Benny/Documents/Fern/aqi_thailand2/
     df = df.drop('date',axis=1)
     df['is_holiday'] = ~df['name'].isna()*1
     return df.drop('name',axis=1).set_index('datetime')
+
+def add_calendar_info(df, holiday_file='C:/Users/Benny/Documents/Fern/aqi_thailand2/data/th_holiday.csv'):
+    """ Add information related to calendar such as holiday, is_weekend, day of week and time of day. 
+    
+    Args:
+        df: data frame with datetime index, 
+    """
+    # add datetime information 
+    df = add_is_holiday(df, holiday_file)
+    # add weekend information 
+    df['is_weekend'] = df.index.dayofweek.isin([5,6])*1
+    # add day of week
+    df['day_of_week'] = df.index.dayofweek
+    df['time_of_day'] = df.index.hour
+    
+    return df
+    
+def wind_to_dummies(series):
+    """One hot encode wind direction columns and group major wind direction
+    
+    """
+    series = series.astype('category')
+    dummies = pd.get_dummies(series)
+
+    # group the wind direction into major wind direction 
+    direction_to_collpse = ['ENE', 'ESE', 'NE', 'NNE', 'NNW', 'NW', 'SE', 'SSE', 'SSW', 'SW',  'WNW', 'WSW']
+
+    for direction in direction_to_collpse:
+        if len(direction)>1:
+            for char in set(direction):
+                dummies[char] = dummies[char] + dummies[direction]
+            dummies.drop(direction, axis=1,inplace=True)
+
+    # group the 'var' direction
+    major_direction = ['E','N','S','W']
+    for direction in major_direction:
+        dummies[direction] = dummies[direction] + dummies['VAR']
+    dummies.drop('VAR',axis=1,inplace=True)
+    dummies.columns = ['wind_' + s for s in dummies.columns ]
+    return dummies
+
+def add_is_rain(df,rain_list=['Rain','Shower','Thunder','Strom','Drizzle']):
+    df['is_rain'] = df['Condition'].str.contains('|'.join(rain_list))*1
+    df = df.drop('Condition', axis=1)
+    return df
+
+
