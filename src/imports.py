5eb777bc11d3af483b4cdda4df9bd15388108dd5
worasom <worasom@gmail.com>
2020-06-07 13:22:17 -0700

    2020-06-07 13:22 auto backup

--- src/imports.py
+++ src/imports.py
@@ -47,6 +47,7 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from scipy.cluster import hierarchy as hc
+from tpot import TPOTRegressor
 
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import pacf
