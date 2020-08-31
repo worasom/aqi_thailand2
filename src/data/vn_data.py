54552c5626a910bd0f79298c88ceb4de76af420f
worasom <worasom@gmail.com>
2020-08-31 10:30:27 -0700

    2020-08-31 10:30 auto backup

---
+++ src/data/vn_data.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+from ..imports import *
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+
+def extract_vn_data(browser, wait_time=10):
+    """Extract hourly data from a Vietnamese EPA station
+    
+    Args:
+        browser: firefox object
+        wait_time: time to wait after the click
+    
+    Returns: dataframe
+    
+    """
+    
+    df = []
+    
+    for i in range(3):
+    
+        # find number of pages to click
+        page = browser.page_source
+        soup = BeautifulSoup(page, features="lxml")
+        # read the first table 
+        df.append(pd.read_html(str(soup))[3])
+        # click the next page
+        browser.find_elements_by_xpath('//*[@id="custom_datatable_1_next"]/a')[0].click()
+        time.sleep(wait_time)
+        
+    # reset the page to the original 
+    back_button = browser.find_elements_by_xpath('//*[@id="custom_datatable_1_previous"]/a')[0]
+    back_button.click()
+    time.sleep(5)
+    back_button.click()
+    time.sleep(5)
+    df = pd.concat(df, ignore_index=True)
+    
+    # convert the data columns and datetime format
+    df = df = df.drop('#', axis=1)
+
+    replace_dict = {'PM-10':'PM10', 'PM-2-5':'PM2.5','Ngày giờ':'datetime', 'VN_AQI giờ':'VN_AQI' }
+    col = pd.DataFrame(df.columns) 
+    col = col.replace(replace_dict).iloc[:,0].to_list()
+    df.columns = col
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    
+    return df.drop_duplicates()
+
+def extract_vn_stations(browser, url= 'http://enviinfo.cem.gov.vn/', wait_time=10):
+    """Get a list of Vietnamese monitoring station 
+    
+    Args:
+        browser:
+        url: website with the station informations
+        wait_time: time to wait after the click
+    
+    Returns: list
+        a list of all stations
+        
+    """
+    
+    page = browser.page_source
+    soup = BeautifulSoup(page, features="lxml")
+    
+    station_list = []
+    for child in soup.find_all(id="cbbStations")[0].children:
+        station = child.text 
+        station_list.append(station)
+    
+    return station_list[1:]
+
+def select_vn_station(browser, station,wait_time=20):
+    """Select the name of the station and wait. 
+
+    """
+    # get the search box 
+    inputelement = browser.find_element_by_xpath('//*[@id="cbbStations_chosen"]/div/div/input')
+    inputelement.send_keys(station)
+    inputelement.send_keys(Keys.RETURN)
+    time.sleep(wait_time)
+
+def extract_station_info(browser):
+    """Extract station name, address and location from the website. 
+    Use after loading the station data up. 
+
+    Returns: dictionary of station information
+
+    """
+    # extract city information 
+    city = browser.find_elements_by_xpath('//*[@id="station_province"]')[0].text.split(': ')[-1]
+    address = browser.find_elements_by_xpath('//*[@id="station_address"]')[0].text.split(': ')[-1]
+    long, lat = browser.find_elements_by_xpath('//*[@id="longlat"]')[0].text.split(' - ')
+    long = long.split(': ')[-1]
+    lat = lat.split(': ')[-1]
+
+    return {'city_vn': city, 'address': address, 'longitude':long, 'latitude':lat}
+
+
+def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_epa/'):
+    """Download hourly data for all monitoring stations from Vietnamese EPA. 
+
+    All data are saved into the save files 
+
+    Args:
+        url: link to the data website
+        save_folder: folder to save the data to 
+
+    """
+    # open the webpage and wait
+    # the page sometimes is slow, so need to wait a bit.
+    browser = webdriver.Firefox()
+    browser.get(url)
+    time.sleep(60)
+
+    # prepare filename and turn to absolute path 
+    date = datetime.now().strftime('%Y-%m-%d_%H-%M')
+    filename = os.path.abspath(f'{save_folder}/{date}.csv')
+    print('save file as ', filename)
+
+    # extract stations name 
+    station_list =  extract_vn_stations(browser)
+    print(f'number of station {len(station_list)}. Takes about 1 mins per station')
+
+    city_dict = {'Hà Nội': 'Hanoi',
+                'Phú Thọ':'Phu Tho',
+                'Quảng Ninh':'Quang Ninh',
+                'Thừa Thiên Huế': 'Thua Thien Hue',
+                'Bắc Ninh': 'Bac Ninh',
+                'Thái nguyên': 'Thai Nguyen',
+                'Gia Lai':'Gia Lai',
+                'Cao Bằng': 'Cao Bang'}
+    
+    data = []
+    station_info_list = []
+
+    for station in station_list:
+        print('working on station', station)
+        # select station 
+        select_vn_station(browser, station, wait_time=20)
+        city_name = station.split(':')[0]
+        # change city_name to english 
+        if city_name in city_dict.keys():
+            city_name = city_dict[city_name]
+
+        station_info = extract_station_info(browser)
+        station_info['city'] = city_name
+        station_info_list.append(station_info)
+
+        # extract data 
+        df = extract_vn_data(browser, wait_time=10)
+        df['city'] = city_name
+        df['station'] = station
+        print('station len ', len(df))
+        data.append(df)
+
+    station_len = len(df)
+
+    print('the last station has len', station_len )
+
+    browser.close()
+
+    return data, station_info_list 
+
+
+
+
+
