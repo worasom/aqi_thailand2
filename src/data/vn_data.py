716246ba8e26760d38d649aea3b8a780cdaea0e6
worasom <worasom@gmail.com>
2020-08-31 21:04:31 -0700

    2020-08-31 21:04 auto backup

--- src/data/vn_data.py
+++ src/data/vn_data.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
-from ..imports import *
+import os
+import sys
+from pathlib import Path
+from tqdm import tqdm, tqdm_notebook
+import json
+import numpy as np
+import pandas as pd
+from glob import glob
+
+# webscraping
+import requests
+import wget
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+import time
+from datetime import datetime, date, timedelta
+
+
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 
@@ -99,7 +117,7 @@ def extract_station_info(browser):
     return {'city_vn': city, 'address': address, 'longitude':long, 'latitude':lat}
 
 
-def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_epa/'):
+def download_vn_data(url='http://enviinfo.cem.gov.vn/', save_folder='../data/vn_epa/'):
     """Download hourly data for all monitoring stations from Vietnamese EPA. 
 
     All data are saved into the save files 
@@ -111,14 +129,16 @@ def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_e
     """
     # open the webpage and wait
     # the page sometimes is slow, so need to wait a bit.
-    browser = webdriver.Firefox()
+
+    executable_path=f'C:/Users/Benny/Documents/Fern/aqi_thailand2/notebooks/geckodriver.exe'
+    browser = webdriver.Firefox(executable_path=executable_path)
     browser.get(url)
     time.sleep(60)
 
     # prepare filename and turn to absolute path 
     date = datetime.now().strftime('%Y-%m-%d_%H-%M')
-    filename = os.path.abspath(f'{save_folder}/{date}.csv')
-    meta_filename = os.path.abspath(f'{save_folder}/stations_info.json')
+    filename = os.path.abspath(f'{save_folder}{date}.csv')
+    meta_filename = os.path.abspath(f'{save_folder}stations_info.json')
 
     # extract stations name 
     station_list =  extract_vn_stations(browser)
@@ -136,7 +156,7 @@ def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_e
     data = []
     station_info_list = []
 
-    for station in tqdm_notebook(station_list):
+    for station in tqdm(station_list):
         # select station 
         select_vn_station(browser, station, wait_time=20)
         city_name = station.split(':')[0]
@@ -147,7 +167,7 @@ def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_e
         station_info = extract_station_info(browser)
         station_info['city'] = city_name
         station_info_list.append(station_info)
-
+  
         # extract data 
         df = extract_vn_data(browser, wait_time=10)
         df['city'] = city_name
@@ -155,23 +175,24 @@ def download_vn_data(url='http://enviinfo.cem.gov.vn/',save_folder='../data/vn_e
         #print(station, 'has data len ', len(df))
         data.append(df)
 
+    browser.close()
+
     station_len = len(df)
     #print('the last station has len', station_len )
     data = pd.concat(data, ignore_index=True)
     print('the total data has len', len(data) )
     print('save file as ', filename)
     data.to_csv(filename, index=False)
-    browser.close()
 
     with open(meta_filename, 'w') as f:
         json.dump(station_info_list, f)
 
     return data, station_info_list 
-    
+
 
 if __name__ == '__main__':
 
-    download_vn_data()
+    download_vn_data(save_folder='../../data/vn_epa/')
 
 
 
